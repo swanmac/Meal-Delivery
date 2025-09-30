d1147d4deedac06009ff588c8009c0aeb8d5b26b
Greg Swan <gmswan99@gmail.com>
2025-09-30 08:58:47 -0400

    create mealsView

---
+++ Meal Delivery/MealsView.swift
@@ -0,0 +1,56 @@
+//
+//  MealsView.swift
+//  Meal Delivery
+//
+//  Created by Greg Swan on 9/30/25.
+//
+
+import SwiftUI
+
+struct MealsView: View {
+    
+    @State var meals = [Meal]()
+    
+    var body: some View {
+        
+        GeometryReader { proxy in
+        
+            ScrollView (showsIndicators: false){
+                
+                VStack (alignment: .leading) {
+                    
+                    Text("Our Meals")
+                        .font(.largeTitle)
+                        .bold()
+                    
+                    LazyVGrid(columns:
+                    [GridItem(), GridItem()],
+                    spacing: 20) {
+                        
+                        ForEach (meals) {m in
+                        
+                            Image(m.imageName)
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .clipShape(RoundedRectangle(cornerRadius: 5))
+                                    .frame(width: (proxy.size.width - 40/2))
+                        }
+                    }
+                        
+                }
+            }
+            
+        }
+                                    .onAppear {
+                                        meals = DataService().getTestData()
+                                    }
+        
+        
+    }
+    
+    
+}
+                                           
+#Preview {
+    ContentView()
+}
