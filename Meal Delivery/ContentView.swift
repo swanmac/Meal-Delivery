a2e0513f2205e2dc4721c6cf9ead1a729b0ff49f
Greg Swan <gmswan99@gmail.com>
2025-09-30 06:20:09 -0400

    Initial Commit

---
+++ Meal Delivery/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Meal Delivery
+//
+//  Created by Greg Swan on 9/30/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Meal Delivery/Meal_DeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meal_DeliveryApp.swift
+//  Meal Delivery
+//
+//  Created by Greg Swan on 9/30/25.
+//
+
+import SwiftUI
+
+@main
+struct Meal_DeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
